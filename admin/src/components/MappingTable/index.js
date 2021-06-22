ad68b92e2b25fdeab9591637e7f4f68e80e82ff0
afonsobspinto <afonsobspinto@gmail.com>
2021-06-22 20:17:06 +0100

    Fix mapping table ui bug

--- admin/src/components/MappingTable/index.js
+++ admin/src/components/MappingTable/index.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import PropTypes from "prop-types";
 import MappingOptions from "./MappingOptions";
 import TargetFieldSelect from "./TargetFieldSelect";
 import CollectionFieldSelect from "./CollectionFieldSelect";
 import {ID_MAPPING, IMPORT_STATE, MATCH_ON_KEY} from "../../utils/constants"
 import _, {get, has} from "lodash";
-import { Table } from "@buffetjs/core";
+import {Table} from "@buffetjs/core";
 import {
   Bool as BoolIcon,
   Json as JsonIcon,
@@ -25,7 +25,7 @@ const headersMapping = {
 class MappingTable extends Component {
 
 
-  state = { mapping: {} };
+  state = {mapping: {}};
 
   getRelatedModel = (fieldName) => {
     const {mapping} = this.state
@@ -35,24 +35,24 @@ class MappingTable extends Component {
     return models.find(model => model.apiID === modelName);
   };
 
-  CustomRow = ({ row }) => {
+  CustomRow = ({row}) => {
 
-    const { fieldName, count, format, minLength, maxLength, meanLength } = row;
+    const {fieldName, count, format, minLength, maxLength, meanLength} = row;
     const showExtraCols = this.props.importState === IMPORT_STATE.relations
     return (
-      <tr style={{ paddingTop: 18 }}>
+      <tr style={{paddingTop: 18}}>
         <td>{fieldName}</td>
         <td>
           <p>{count}</p>
         </td>
         <td>
-          {format === "string" && <TextIcon fill="#fdd835" />}
-          {format === "number" && <NumberIcon fill="#fdd835" />}
-          {format === "boolean" && <BoolIcon fill="#fdd835" />}
-          {format === "object" && <JsonIcon fill="#fdd835" />}
-          {format === "email" && <EmailIcon fill="#fdd835" />}
-          {format === "date" && <DateIcon fill="#fdd835" />}
-          {format === "xml" && <XmlIcon fill="#fdd835" />}
+          {format === "string" && <TextIcon fill="#fdd835"/>}
+          {format === "number" && <NumberIcon fill="#fdd835"/>}
+          {format === "boolean" && <BoolIcon fill="#fdd835"/>}
+          {format === "object" && <JsonIcon fill="#fdd835"/>}
+          {format === "email" && <EmailIcon fill="#fdd835"/>}
+          {format === "date" && <DateIcon fill="#fdd835"/>}
+          {format === "xml" && <XmlIcon fill="#fdd835"/>}
           <p>{format}</p>
         </td>
         <td>
@@ -80,31 +80,31 @@ class MappingTable extends Component {
             />
           )}
         </td>
-        <td>
-          {showExtraCols && this.props.models && (
+        {showExtraCols && this.props.models && (
+          <td>
             <CollectionFieldSelect
               models={this.props.models}
               onChange={targetCollection => this.setMapping(fieldName, targetCollection, headersMapping.COLLECTION)}
             />
-          )}
-        </td>
-        <td>
-          {showExtraCols && this.props.models && (
+          </td>
+        )}
+        {showExtraCols && this.props.models && (
+          <td>
             <TargetFieldSelect
               targetModel={this.getRelatedModel(fieldName)}
               onChange={targetCollection => this.setMapping(fieldName, targetCollection, headersMapping.COLLECTION_COL)}
               defaultLabel={ID_MAPPING}
             />
-          )}
-        </td>
+          </td>
+        )}
       </tr>
     );
   };
   changeMappingOptions = stat => options => {
     let newState = _.cloneDeep(this.state);
-    if(options[MATCH_ON_KEY]){
-      for(let key in newState['mapping']){
-        if(newState['mapping'][key][MATCH_ON_KEY]){
+    if (options[MATCH_ON_KEY]) {
+      for (let key in newState['mapping']) {
+        if (newState['mapping'][key][MATCH_ON_KEY]) {
           delete newState['mapping'][key][MATCH_ON_KEY]
         }
       }
@@ -125,33 +125,33 @@ class MappingTable extends Component {
   };
 
   getHeaders = () => {
-    const {importState } = this.props;
+    const {importState} = this.props;
 
     return importState === IMPORT_STATE.content ? [
-      { name: "Field", value: "fieldName" },
-      { name: "Count", value: "count" },
-      { name: "Format", value: "format" },
-      { name: "Min Length", value: "minLength" },
-      { name: "Max Length", value: "maxLength" },
-      { name: "Mean Length", value: "meanLength" },
-      { name: "Options", value: "options" },
-      { name: "Destination", value: "destination" }
+      {name: "Field", value: "fieldName"},
+      {name: "Count", value: "count"},
+      {name: "Format", value: "format"},
+      {name: "Min Length", value: "minLength"},
+      {name: "Max Length", value: "maxLength"},
+      {name: "Mean Length", value: "meanLength"},
+      {name: "Options", value: "options"},
+      {name: "Destination", value: "destination"}
     ] : [
-      { name: "Field", value: "fieldName" },
-      { name: "Count", value: "count" },
-      { name: "Format", value: "format" },
-      { name: "Min Length", value: "minLength" },
-      { name: "Max Length", value: "maxLength" },
-      { name: "Mean Length", value: "meanLength" },
-      { name: "Options", value: "options" },
-      { name: "Destination Column", value: headersMapping.DESTINATION },
-      { name: "Related Collection", value: headersMapping.COLLECTION},
-      { name: "Related Column", value: headersMapping.COLLECTION_COL }
+      {name: "Field", value: "fieldName"},
+      {name: "Count", value: "count"},
+      {name: "Format", value: "format"},
+      {name: "Min Length", value: "minLength"},
+      {name: "Max Length", value: "maxLength"},
+      {name: "Mean Length", value: "meanLength"},
+      {name: "Options", value: "options"},
+      {name: "Destination Column", value: headersMapping.DESTINATION},
+      {name: "Related Collection", value: headersMapping.COLLECTION},
+      {name: "Related Column", value: headersMapping.COLLECTION_COL}
     ];
   }
 
   render() {
-    const { analysis} = this.props;
+    const {analysis} = this.props;
     const props = {
       title: "Field Mapping",
       subtitle:
