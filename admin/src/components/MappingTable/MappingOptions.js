3b7878fe64560cb2691e66370d70e48d630f716a
afonsobspinto <afonsobspinto@gmail.com>
2021-08-03 17:57:58 +0100

    #77 Add default match on

--- admin/src/components/MappingTable/MappingOptions.js
+++ admin/src/components/MappingTable/MappingOptions.js
@@ -1,9 +1,10 @@
 import React from "react";
 import TargetFieldSelect from "./TargetFieldSelect";
 import {Label} from "@buffetjs/core";
-import {IMPORT_STATE, MATCH_ON_KEY} from "../../utils/constants";
+import {IMPORT_STATE, MATCH_ON_KEY, REL_COL_ID} from "../../utils/constants";
 
 const MappingOptions = ({stat, onChange, targetModel, importState}) => {
+  const matchOnChecked = stat.fieldName === REL_COL_ID ? 'checked' : null
   return importState === IMPORT_STATE.content ? (
       <div>
         {stat.format === "xml" && (
@@ -35,6 +36,7 @@ const MappingOptions = ({stat, onChange, targetModel, importState}) => {
         name={"MatchOnIDRadio"}
         type="radio"
         onChange={e => onChange({[MATCH_ON_KEY]: e.target.checked})}
+        checked={matchOnChecked}
       />
     </div>
 
